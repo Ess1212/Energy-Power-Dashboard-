dc202ba7f9e8ed7dffdcd8066137e155ce47ece9
Ess1212 <schneitech.essteam2025@gmail.com>
2025-12-20 07:53:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1503,16 +1503,3 @@ st.markdown('</div>', unsafe_allow_html=True)
 # =============================================================================
 # END PART 5
 # =============================================================================
-[gcp_service_account]
-type = "service_account"
-project_id = "your-project-id"
-private_key_id = "xxxxx"
-private_key = "-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----\n"
-client_email = "xxxx@xxxx.iam.gserviceaccount.com"
-client_id = "xxxx"
-auth_uri = "https://accounts.google.com/o/oauth2/auth"
-token_uri = "https://oauth2.googleapis.com/token"
-auth_provider_x509_cert_url = "https://www.googleapis.com/oauth2/v1/certs"
-client_x509_cert_url = "https://www.googleapis.com/robot/v1/metadata/x509/..."
-
-
